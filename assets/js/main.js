4e825957a31b5f7abe21d1faecba9ac326d10b6b
Yusuf Incesu <yusufandscad@gmail.com>
2024-04-16 18:45:31 +0300

    skills and scroll bug fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,24 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  // Add a class if the bottom offset is greater than 50 of the viewport
+  this.scrollY >= 50
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== SERVICES MODAL ===============*/
 
-
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
-
-/* Link active work */ 
-
+/* Link active work */
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
-
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== LIGHT DARK THEME ===============*/ 
-
+/*=============== LIGHT DARK THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-
