d20f1c86ebda9ea3c37f9304f104496fc1ea7aa2
Yusuf Incesu <yusufandscad@gmail.com>
2024-04-19 12:50:49 +0300

    add email.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,6 +98,49 @@ let swiper = new Swiper(".testimonial__container", {
   },
 });
 
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form"),
+  contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  // Form alanlarını al
+  const userName = contactForm.elements["user_name"].value.trim();
+  const userEmail = contactForm.elements["user_email"].value.trim();
+  const userMessage = contactForm.elements["user_mesage"].value.trim();
+
+  // Alanların boş olup olmadığını kontrol et
+  if (userName === "" || userEmail === "" || userMessage === "") {
+    contactMessage.textContent = "Please fill out all fields ❌";
+    return;
+  }
+
+  emailjs
+    .sendForm(
+      "service_9yjqgr6",
+      "template_5yjbq1h",
+      "#contact-form",
+      "U7eEM3ZXfKe2TDN3-"
+    )
+    .then(
+      () => {
+        contactMessage.textContent = "Message sent successfully ✅";
+        // Formu temizle
+        contactForm.reset();
+        // 3 saniye sonra mesajı gizle
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 3000);
+      },
+      () => {
+        contactMessage.textContent = "Message not sent (service error) ❌ ";
+      }
+    );
+};
+
+contactForm.addEventListener("submit", sendEmail);
+
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 const sections = document.querySelectorAll("section[id]");
